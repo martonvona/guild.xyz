3b1172694cb63f717a41054ddce8a76d6df7bf24
Marton Vona <martonvona@gmail.com>
2023-06-08 11:29:31 +0200

    feat: implement new drag and drop logic on pair the logo minigame

--- src/components/guess-the-guild/DropableGuildCrad.tsx
+++ src/components/guess-the-guild/DropableGuildCrad.tsx
@@ -1,6 +1,5 @@
 import {
-  Circle,
-  SimpleGrid,
+  Grid,
   Tag,
   TagLabel,
   TagLeftIcon,
@@ -10,86 +9,56 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react"
 import DisplayCard from "components/common/DisplayCard"
-import GuildLogo from "components/common/GuildLogo"
-import { motion } from "framer-motion"
-import { Users } from "phosphor-react"
-import { useState } from "react"
+import { DotsSixVertical, Users } from "phosphor-react"
 import { GuildBase } from "types"
 import pluralize from "utils/pluralize"
-import { Guess } from "./hooks/useDragAndDrop"
 
 type Props = {
   guildData: GuildBase
-  onDragOver: () => void
-  solution: Guess[]
 }
 
-const DropableGuildCard = ({
-  guildData,
-  onDragOver,
-  solution,
-}: Props): JSX.Element => {
-  const border = useColorModeValue("blackAlpha.500", "whiteAlpha.500")
-  const [hover, setHover] = useState(false)
-
-  const guess = solution.find((item) => item.row.id === guildData.id)
+const DropableGuildCard = ({ guildData }: Props): JSX.Element => {
+  const background = useColorModeValue("blackAlpha.100", "whiteAlpha.100")
 
   return (
-    <motion.div
-      onDragOver={() => {
-        onDragOver()
-        setHover(true)
-      }}
-      onDragLeave={() => setHover(false)}
+    <DisplayCard
+      shadow="none"
+      bg={background}
+      borderLeftRadius={{ base: "none", sm: "none" }}
+      mb={2}
+      cursor="grab"
+      pr={{ base: 3, md: 3 }}
     >
-      <DisplayCard>
-        <SimpleGrid
-          templateColumns={"3rem calc(100% - 4.25rem)"}
-          gap={4}
-          alignItems="center"
-        >
-          {guess ? (
-            <GuildLogo imageUrl={guess.guess.imageUrl} />
-          ) : (
-            <Circle
-              size={"3rem"}
-              __css={{
-                borderWidth: "2px",
-                borderColor: hover ? "purple.500" : border,
-                borderStyle: "dashed",
-              }}
-            />
-          )}
-          <VStack spacing={2} alignItems="start" w="full" maxW="full" mb="1" mt="-1">
-            <Text
-              as="span"
-              fontFamily="display"
-              fontSize="xl"
-              fontWeight="bold"
-              letterSpacing="wide"
-              maxW="full"
-              noOfLines={1}
-              color={hover ? "purple.500" : "inherit"}
-            >
-              {guildData.name}
-            </Text>
-            <Wrap zIndex="1">
-              <Tag as="li">
-                <TagLeftIcon as={Users} />
-                <TagLabel>
-                  {new Intl.NumberFormat("en", { notation: "compact" }).format(
-                    guildData.memberCount ?? 0
-                  )}
-                </TagLabel>
-              </Tag>
-              <Tag as="li">
-                <TagLabel>{pluralize(guildData.rolesCount ?? 0, "role")}</TagLabel>
-              </Tag>
-            </Wrap>
-          </VStack>
-        </SimpleGrid>
-      </DisplayCard>
-    </motion.div>
+      <Grid templateColumns={"7fr 1fr"} gap={4} alignItems="center">
+        <VStack spacing={2} alignItems="start" w="full" maxW="full" mb="1" mt="-1">
+          <Text
+            as="span"
+            fontFamily="display"
+            fontSize="xl"
+            fontWeight="bold"
+            letterSpacing="wide"
+            maxW="full"
+            noOfLines={1}
+          >
+            {guildData.name}
+          </Text>
+          <Wrap zIndex="1">
+            <Tag as="li">
+              <TagLeftIcon as={Users} />
+              <TagLabel>
+                {new Intl.NumberFormat("en", { notation: "compact" }).format(
+                  guildData.memberCount ?? 0
+                )}
+              </TagLabel>
+            </Tag>
+            <Tag as="li">
+              <TagLabel>{pluralize(guildData.rolesCount ?? 0, "role")}</TagLabel>
+            </Tag>
+          </Wrap>
+        </VStack>
+        <DotsSixVertical size={32} />
+      </Grid>
+    </DisplayCard>
   )
 }
 
--- src/components/guess-the-guild/PairGame.tsx
+++ src/components/guess-the-guild/PairGame.tsx
@@ -1,12 +1,19 @@
-import { Card, Heading, SimpleGrid, useColorMode } from "@chakra-ui/react"
+import {
+  Box,
+  Flex,
+  Grid,
+  Heading,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react"
 import Button from "components/common/Button"
 import ColorCard from "components/common/ColorCard/ColorCard"
 import GuildLogo from "components/common/GuildLogo"
-import { motion } from "framer-motion"
+import { Reorder } from "framer-motion"
 import image from "next/image"
+import { useEffect, useState } from "react"
 import { GuildBase } from "types"
 import DropableGuildCard from "./DropableGuildCrad"
-import usePairGame from "./hooks/usePairGame"
 
 type Props = {
   guilds: GuildBase[]
@@ -14,79 +21,67 @@ type Props = {
 
 const PairGame = ({ guilds }: Props): JSX.Element => {
   const { colorMode } = useColorMode()
-  const {
-    onDragStart,
-    onDragEnd,
-    check,
-    onDragOver,
-    logos,
-    rows,
-    removeLogo,
-    solution,
-    reset,
-  } = usePairGame(guilds)
+  const options: GuildBase[] = guilds.slice(0, 4)
+  const [guildIds, setGuildIds] = useState<number[]>([])
+  const border = useColorModeValue("blackAlpha.100", "whiteAlpha.100")
+
+  useEffect(() => {
+    setGuildIds(shuffle(options).map((guild) => guild.id))
+  }, [])
 
   return (
     <ColorCard color="transparent" colorMode={colorMode} w="fit-content">
       <Heading as="h2" size="md" mb={4}>
         Pair the logos to their guilds
       </Heading>
-      <Card
-        align={"center"}
-        justify={"center"}
-        direction={"row"}
-        gap={3}
-        p={4}
-        shadow={"none"}
-      >
-        {logos.map((guild) => (
-          <motion.div
-            drag
-            key={guild.imageUrl}
-            draggable
-            onDragStartCapture={() => onDragStart(guild)}
-            onDragEnd={() => {
-              onDragEnd()
-              removeLogo(guild)
-            }}
-            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-          >
-            {image && <GuildLogo imageUrl={guild.imageUrl} />}
-          </motion.div>
-        ))}
-      </Card>
-      <SimpleGrid spacing={2}>
-        {rows.map((guild) => (
-          <DropableGuildCard
-            solution={solution}
-            key={guild.id}
-            guildData={guild}
-            onDragOver={() => onDragOver(guild)}
-          />
-        ))}
-      </SimpleGrid>
-      <SimpleGrid spacing={2} columns={2} pt={5}>
-        <Button
-          h="10"
-          colorScheme="purple"
-          onClick={() => {
-            reset()
-          }}
-        >
-          Reset
-        </Button>
-        <Button
-          h="10"
-          colorScheme="green"
-          onClick={() => {
-            console.log(check())
-          }}
-        >
-          Submit
-        </Button>
-      </SimpleGrid>
+      <Grid templateColumns={"2fr 5fr"} gap={1}>
+        <Flex direction={"column"} gap={2} pb={2}>
+          {options.map((option) => (
+            <Box
+              key={option.id}
+              h="full"
+              w="full"
+              display={"flex"}
+              border={"3px dashed"}
+              borderRight={"none"}
+              justifyContent={"center"}
+              borderRadius={"2xl"}
+              borderRightRadius={{ base: "none", sm: "none" }}
+              alignItems={"center"}
+              borderColor={border}
+              m={0}
+            >
+              {image && <GuildLogo imageUrl={option.imageUrl} />}
+            </Box>
+          ))}
+        </Flex>
+        <Reorder.Group axis="y" values={guildIds} onReorder={setGuildIds}>
+          {guildIds?.map((id) => (
+            <Reorder.Item key={id} value={id}>
+              <DropableGuildCard
+                guildData={options.find((option) => id === option.id)}
+              />
+            </Reorder.Item>
+          ))}
+        </Reorder.Group>
+      </Grid>
+
+      <Button h="10" colorScheme="green" onClick={() => {}}>
+        Submit
+      </Button>
     </ColorCard>
   )
+}
+
+function shuffle(array) {
+  const newArray = [...array]
+
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[newArray[i], newArray[j]] = [newArray[j], newArray[i]]
+  }
+
+  return newArray
 }
 
 export default PairGame
